50b51803318bfe793c64076c75c9c3d5ade31cae
GanjaGanja <ochenj.gorkyj@users.noreply.github.com>
2017-03-26 19:07:59 +0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
